d888341db3b0575f4fc1c143ba71b07ce10b285a
angelo <angelo.pjsol@gmail.com>
2020-08-03 23:47:36 -0300

    points page - map done

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,34 +1,42 @@
 import React from 'react';
-import { Feather as Icon, AntDesign as Emoji } from '@expo/vector-icons'
+
+import { Feather as Icon } from '@expo/vector-icons'
 import { View, ImageBackground, Image, Text, StyleSheet } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 
+import { useNavigation } from '@react-navigation/native';
+
 const Home = () => {
-    return (
-        <ImageBackground 
-            source={require('../../assets/home-background.png')}
-            style={styles.container}
-            imageStyle={{ width: 274, height: 368 }}
-        >
-            <View style={styles.main}>
-                <Image source={require('../../assets/logo.png')} />
-                <Text style={styles.title}>Olá amigos do Linkedin! <Emoji name='smileo' color='#322153' size={24} /></Text>
-                <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de cleta de forma eficiente</Text>
-            </View>
-            <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={ () => {}}>
-                    <View style={styles.buttonIcon}>
-                        <Text>
-                            <Icon name='arrow-right' color='#FFF' size={24} />   
-                        </Text>
-                    </View>
-                    <Text style={styles.buttonText}>
-                        Entrar
+  const navigation = useNavigation();
+  function handleNavigateToPoints() {
+    navigation.navigate('Points');
+  }
+
+  return (
+    <ImageBackground 
+        source={require('../../assets/home-background.png')}
+        style={styles.container}
+        imageStyle={{ width: 274, height: 368 }}
+    >
+        <View style={styles.main}>
+            <Image source={require('../../assets/logo.png')} />
+            <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+            <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de cleta de forma eficiente</Text>
+        </View>
+        <View style={styles.footer}>
+            <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+                <View style={styles.buttonIcon}>
+                    <Text>
+                        <Icon name='arrow-right' color='#FFF' size={24} />   
                     </Text>
-                </RectButton>
-            </View>
-        </ImageBackground>
-    )
+                </View>
+                <Text style={styles.buttonText}>
+                    Entrar
+                </Text>
+            </RectButton>
+        </View>
+    </ImageBackground>
+  )
 };
 
 export default Home;
